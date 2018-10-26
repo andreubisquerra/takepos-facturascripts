cf161ffda3f33efd4e9f7dcb1a4e1c93e6210466
andreubisquerra <jove@bisquerra.com>
2018-10-26 09:36:48 +0200

    Creating module

---
+++ Controller/Takepos.php
@@ -0,0 +1,23 @@
+<?php
+namespace FacturaScripts\Plugins\takepos\Controller;
+
+use FacturaScripts\Core\Base\Controller;
+
+class Takepos extends Controller
+{
+    public function getPageData()
+    {
+        $pageData = parent::getPageData();
+        $pageData['title'] = 'TakePOS';
+        $pageData['menu'] = 'TakePOS';
+        $pageData['icon'] = 'fas fa-page';
+
+        return $pageData;
+    }
+
+    public function privateCore(&$response, $user, $permissions)
+    {
+        parent::privateCore($response, $user, $permissions);
+		$this->setTemplate('Wizard');
+    }
+}
